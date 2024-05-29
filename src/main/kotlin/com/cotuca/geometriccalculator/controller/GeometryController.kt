ae3b599ead88fc51955b6288f7a01bffc8047feb
PAULO HYAGO DA SILVEIRA YAMANACA <yagoyamanaka25@gmail.com>
2024-05-29 18:40:38 -0300

    rotas formas

--- src/main/kotlin/com/cotuca/geometriccalculator/controller/GeometryController.kt
+++ src/main/kotlin/com/cotuca/geometriccalculator/controller/GeometryController.kt
@@ -9,8 +9,33 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("geometric")
 class GeometryController {
 
-    @GetMapping
-    fun list() {
 
+    @GetMapping("/circle")
+    fun getCircle(@RequestParam radius: Double): Map<String, Double> {
+        val perimeter = 2 * PI * radius
+        val area = PI * radius.pow(2)
+        return mapOf("perimeter" to perimeter, "area" to area)
+    }
+
+    @GetMapping("/cone")
+    fun getCone(@RequestParam radius: Double, @RequestParam height: Double): Map<String, Double> {
+        val slantHeight = sqrt(radius.pow(2) + height.pow(2))
+        val surfaceArea = PI * radius * (radius + slantHeight)
+        val volume = (1.0 / 3) * PI * radius.pow(2) * height
+        return mapOf("surfaceArea" to surfaceArea, "volume" to volume)
+    }
+
+    @GetMapping("/hexagon")
+    fun getHexagon(@RequestParam side: Double): Map<String, Double> {
+        val perimeter = 6 * side
+        val area = (3 * sqrt(3.0) / 2) * side.pow(2)
+        return mapOf("perimeter" to perimeter, "area" to area)
+    }
+
+    @GetMapping("/square")
+    fun getSquare(@RequestParam side: Double): Map<String, Double> {
+        val perimeter = 4 * side
+        val area = side.pow(2)
+        return mapOf("perimeter" to perimeter, "area" to area)
     }
 }
